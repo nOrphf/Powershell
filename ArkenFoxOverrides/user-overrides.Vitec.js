c2c914860788d8715ee11841d4de705754601189
nOrphf <54571822+nOrphf@users.noreply.github.com>
2024-08-04 18:41:52 +0200

    Update user-overrides.Vitec.js for bug check

--- ArkenFoxOverrides/user-overrides.Vitec.js
+++ ArkenFoxOverrides/user-overrides.Vitec.js
@@ -35,7 +35,7 @@ user_pref("browser.search.separatePrivateDefault.ui.enabled", false); // [FF71+]
 /* 0906: enforce no automatic authentication on Microsoft sites [FF91+] [WINDOWS 10+]
  * [SETTING] Privacy & Security>Logins and Passwords>Allow Windows single sign-on for...
  * [1] https://support.mozilla.org/kb/windows-sso ***/
-user_pref("network.http.windows-sso.enabled", true); // [DEFAULT: false]
+user_pref("network.http.windows-sso.enabled", false); // [DEFAULT: false]
 /* 1703: set external links to open in site-specific containers [FF123+]
  * [SETUP-WEB] Depending on your container extension(s) and their settings
  * true=Firefox will not choose a container (so your extension can)
@@ -299,7 +299,5 @@ user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-f
 user_pref("browser.uidensity", 1);
 user_pref("floorp.newtab.overrides.newtaburl", "https://www.startpage.com/do/mypage.pl?prfe=b1c1900c2cfd73603ac4d1a7d98a987dde7b1cf345206497a62c5d546801bda1b9e7bb0d91e763990790f59c101b15109d1f3e655f6fe39328c09331547b0a4d01dfa16e6ccabb006d55789f79a8");
 user_pref("browser.toolbars.bookmarks.visibility", "never");
-user_pref("network.trr.excluded-domains", "vitec.se,aloc.com");
-user_pref("network.automatic-ntlm-auth.trusted-uris", "tfs.vitec.se");
 user_pref("browser.display.statusbar", false);
 user_pref("taskbar.grouping.useprofile", true);
