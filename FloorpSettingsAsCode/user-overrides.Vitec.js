1eb610f8c58de0346604f10fbd819a5592f497df
nOrphf <54571822+nOrphf@users.noreply.github.com>
2024-08-10 16:07:52 +0000

    Update user-overrides.Vitec.js

--- FloorpSettingsAsCode/user-overrides.Vitec.js
+++ FloorpSettingsAsCode/user-overrides.Vitec.js
@@ -1,2 +1,3 @@
 user_pref("network.trr.excluded-domains", "vitec.se,aloc.com");
 user_pref("network.automatic-ntlm-auth.trusted-uris", "tfs.vitec.se");
+user_pref("network.http.windows-sso.enabled", true);
